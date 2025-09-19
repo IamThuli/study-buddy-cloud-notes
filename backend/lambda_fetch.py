c06e3afbc02acdb86795d3dc53c7b356fba8a83b
IamThuli <142389666+IamThuli@users.noreply.github.com>
2025-09-19 17:55:08 +0200

    Update lambda_fetch.py

--- backend/lambda_fetch.py
+++ backend/lambda_fetch.py
@@ -1,1 +1,17 @@
 
+import boto3
+import json
+
+s3 = boto3.client('s3')
+BUCKET = "study-buddy-notes-thuli"
+
+def lambda_handler(event, context):
+    note_id = event["queryStringParameters"].get("note_id")
+   
+    obj = s3.get_object(Bucket=BUCKET, Key=note_id)
+    note_text = obj['Body'].read().decode('utf-8')
+   
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"note": note_text})
+    }
